f25e608cf87c883fa0f1a456ceede3261209f7c0
Zhengyang <songzy_thu@163.com>
2026-03-01 14:24:26 +0800

    Refactor code for 2016 round c problem b.

--- kickstart/2016/Round C/B.safe_squares.py
+++ kickstart/2016/Round C/B.safe_squares.py
@@ -1,9 +1,32 @@
 # https://zibada.guru/gcj/ks2016c/problems/#B
+#
+# DP
+# https://www.geeksforgeeks.org/dsa/maximum-size-sub-matrix-with-all-1s-in-a-binary-matrix/
+
+def count_squares(grid, R, C):
+    # 1. count number of squares with bottom right corner at (i, j)
+    dp = [[0 for i in range(C + 1)] for j in range(R + 1)]
+    for i in range(R, -1, -1):
+        for j in range(C, -1, -1):
+            if grid[i][j]:
+                dp[i][j] = 0
+            elif grid[i+1][j] or grid[i][j+1]:
+                dp[i][j] = 1
+            else:
+                # core
+                dp[i][j] = min(dp[i+1][j+1], dp[i+1][j], dp[i][j+1]) + 1
+
+    # 2. sum up all number of squares
+    ans = 0
+    for i in range(R+1):
+        for j in range(C+1):
+            ans += dp[i][j]
+    return ans
+
 
 T = int(input())
 for t in range(1, T+1):
     R, C, K = map(int, input().split())
-    # R, C \in [1, 3000], K \in  [0, 3000]
     grid = [[0 for i in range(C+1)] for j in range(R+1)]
     for i in range(R+1):
         grid[i][-1] = 1
@@ -13,21 +36,5 @@ for t in range(1, T+1):
         r, c = map(int, input().split())
         grid[r][c] = 1
 
-    # the maximum possible side length as the left top corner
-    dp = [[0 for i in range(C + 1)] for j in range(R + 1)]
-
-    for i in range(R, -1, -1):
-        for j in range(C, -1, -1):
-            if grid[i][j]:
-                dp[i][j] = 0
-            elif grid[i+1][j] or grid[i][j+1]:
-                dp[i][j] = 1
-            else:
-                # notice the recursion formula
-                dp[i][j] = min(dp[i+1][j+1], dp[i+1][j], dp[i][j+1]) + 1
-
-    ans = 0
-    for i in range(R+1):
-        for j in range(C+1):
-            ans += dp[i][j]
+    ans = count_squares(grid, R, C)
     print("Case #%d: %d" % (t, ans))
