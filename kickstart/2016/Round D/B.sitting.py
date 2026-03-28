d6dfe3b0e4632e80c7945b74ec8eb58c3c9c7e91
Zhengyang <songzy_thu@163.com>
2026-03-28 13:49:37 +0800

    Update code of Problem B, Round D, 2016.

--- kickstart/2016/Round D/B.sitting.py
+++ kickstart/2016/Round D/B.sitting.py
@@ -1,24 +1,22 @@
 # https://zibada.guru/gcj/ks2016d/problems/#B
 
+def compute(row, col):
+    '''
+    xox xox xox ...
+    xxo xxo xxo ...
+    oxx oxx oxx ...
+    ... ... ... ...
+    '''
+    if row > col:
+        row, col = col, row
+
+    if row <= 2:
+        return row * (col - col // 3)
+    return row * col - row * col // 3
+
+
 T = int(input())
 for t in range(1, T+1):
-    def compute(row, col):
-        if col <= 0:
-            return 0
-        if r <= 2:
-            return r * (c - c / 3)
-        return r * c - r * c / 3
-
     r, c = map(int, input().split())
-    if r > c:
-        r, c = c, r  # this is important!!!
     ans = compute(r, c)
-
     print('Case #%d: %d' % (t, ans))
-
-'''
-xox xox xox ...
-xxo xxo xxo ...
-oxx oxx oxx ...
-... ... ... ...
-'''
