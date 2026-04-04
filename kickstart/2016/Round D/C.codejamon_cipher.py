018de8d58fde7d272f7b5e232720079e77f9f577
Zhengyang <songzy_thu@163.com>
2026-04-04 20:55:37 +0800

    Refactor code of 2016 Round D problem C.

--- kickstart/2016/Round D/C.codejamon_cipher.py
+++ kickstart/2016/Round D/C.codejamon_cipher.py
@@ -1,47 +1,59 @@
 # https://zibada.guru/gcj/ks2016d/problems/#C
 
 from collections import defaultdict
-T = int(input())
-mod = 10**9+7
+
+MOD = 10**9+7
 
 
-for t in range(1, T+1):
-    V, S = map(int, input().split())
+def pos(c):
+    return ord(c)-ord('a')
 
-    def pos(c):
-        return ord(c)-ord('a')
 
-    def get_key(word):
-        k = [0 for i in range(26)]
-        for c in word:
-            k[pos(c)] += 1
-        return tuple(k)
+def get_key(word):
+    k = [0 for i in range(26)]
+    for c in word:
+        k[pos(c)] += 1
+    return tuple(k)
 
+
+def get_ans(sen, m):
+    dp = [1] + [0 for i in range(len(sen))]
+
+    for i in range(0, len(sen)):
+        # i from 0 to len(sen) - 1
+        k = [0 for _ in range(26)]
+        for j in range(i, -1, -1):
+            # consider the substr from j to i
+            k[pos(sen[j])] += 1
+            if tuple(k) in m:
+                dp[i+1] += dp[j] * m[tuple(k)]
+                dp[i+1] %= MOD
+    return dp[-1]
+
+
+def get_ans_list(words, sentences):
     m = defaultdict(int)
-    for v in range(V):
-        word = input()
+    for word in words:
         m[get_key(word)] += 1
 
-    al = []
+    ans_list = []
+    for sentence in sentences:
+        ans_list.append(get_ans(sentence, m))
+    return ans_list
 
-    def get_ans(sen):
-        dp = [1] + [0 for i in range(len(sen))]
 
-        for i in range(0, len(sen)):
-            # i from 0 to len(sen) - 1
-            k = [0 for _ in range(26)]
-            for j in range(i, -1, -1):
-                # consider the substr from j to i
-                k[pos(sen[j])] += 1
-                if tuple(k) in m:
-                    dp[i+1] += dp[j] * m[tuple(k)]
-                    dp[i+1] %= mod
-        return dp[-1]
+if __name__ == '__main__':
+    T = int(input())
+    for t in range(1, T+1):
+        V, S = map(int, input().split())
 
-    for s in range(S):
-        sen = input()
-        al += str(get_ans(sen)),
-    print('Case #%d: %s' % (t, ' '.join(al)))
+        words = []
+        for v in range(V):
+            words.append(input())
 
-# use tuple as key, and do not count them repeatly
-# count them iterately rather than use Counter
+        sentences = []
+        for s in range(S):
+            sentences.append(input())
+
+        ans_list = get_ans_list(words, sentences)
+        print('Case #%d: %s' % (t, ' '.join(map(str, ans_list))))
