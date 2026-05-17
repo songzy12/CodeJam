61799b0031160531dca412c86b042590c00d16a5
Zhengyang <songzy_thu@163.com>
2026-05-17 10:09:02 +0800

    Add solution for Problem D, Round E, 2016.

--- kickstart/2016/Round_E/C.partioning_number.py
+++ kickstart/2016/Round_E/C.partioning_number.py
@@ -1,20 +1,58 @@
-T = int(raw_input())
-for t in range(1, T+1):
-    N, D = map(int, raw_input().split())
-    def compute(N, d):
-    # the first is d
-        ans = 0
-        for x in range(1, N / d + 1):
-            y = N - x * d
-            def compute0(x, y):
-                a = max(x-y, 1)
-                b = min((2*x-y)/2, x)
-                if a > b:
-                    return 0
-                return b - a + 1
-            ans += compute0(x, y) 
-        return ans
+# https://zibada.guru/gcj/ks2016e/problems/#C
+#
+# Find the number of tuples (a, b, c, x) such that:
+#   a*x + b*(x+1) + c*(x+2) = N, where
+#   1. a, b, c, x are non-negative integers
+#   2. D | x
+#   3. a >= 1
+#
+# Also, https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+
+
+def compute_solution_count(N, a, x):
+    """
+    Compute the number of tuples (b, c) such that:
+    1. a*x + b*(x+1) + c*(x+2) = N
+    2. b, c are non-negative integers
+
+    That is:
+
+    (x+1)*b + (x+2)*c = N - a*x
+
+    https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+    https://www.geeksforgeeks.org/python/python-program-for-basic-and-extended-euclidean-algorithms-2/
+    """
+
+    if N - a*x < 0:
+        return 0
+    # Here is how we compute an initial solution (b0, c0).
+    # Note: gcd(x+1, x+2) = 1, that means:
+    #   (-1, 1) * (N - a*x) is a solution of (b, c)
+    #
+    # All solutions would be (-(N-a*x) + t*(x+2), N-a*x - t*(x+1))
+    #
+    # We need to find the number of t such that:
+    # 1. -(N-a*x) + t*(x+2) >= 0
+    # 2. N-a*x - t*(x+1) >= 0
+    # That is:
+    # * (N-a*x) / (x+1) >= t >= (N-a*x) / (x+2)
+    start = (N - a*x) // (x+2)
+    if (N - a*x) % (x+2) != 0:
+        start += 1
+    end = (N - a*x) // (x+1)
+    return end - start + 1
+
+
+def solve(N, D):
     ans = 0
-    for d in range(D, N+1, D):
-        ans += compute(N, d)
-    print 'Case #%d: %d' % (t, ans)
+    for x in range(D, N+1, D):
+        for a in range(1, N//x+1):
+            ans += compute_solution_count(N, a, x)
+    return ans
+
+
+T = int(input())
+for t in range(1, T+1):
+    N, D = map(int, input().split())
+    ans = solve(N, D)
+    print('Case #%d: %d' % (t, ans))
